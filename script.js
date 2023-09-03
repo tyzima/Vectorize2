6101f6e5946caaf6a873aa5ea4714b1fe719fa3c
tyzima <41972512+tyzima@users.noreply.github.com>
2023-09-03 19:57:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,72 +5,66 @@ const vectorizeButton = document.getElementById("vectorize-btn");
 const downloadButton = document.getElementById("download-btn");
 const imageSlider = document.querySelector(".image-slider");
 
-let selectedColor = 0; // Default to no color selected
+let selectedColorCount = 0;
 
-fileInput.addEventListener("change", function() {
-    const file = fileInput.files[0];
-    if (file) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function(e) {
-            const imgElement = document.createElement("img");
-            imgElement.src = e.target.result;
-            imageSlider.innerHTML = ""; // Clear previous images
-            imageSlider.appendChild(imgElement); // Display the "before" image
-        };
+// Function to select the color count
+function selectColor(count) {
+    // Resetting all selections
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById(`color-${i}`).classList.remove('selected');
     }
-});
+    // Setting the selected color count
+    document.getElementById(`color-${count}`).classList.add('selected');
+    selectedColorCount = count;
+}
 
-colorCircles.forEach(circle => {
-    circle.addEventListener("click", function() {
-        // Remove the 'selected' class from all circles
-        colorCircles.forEach(c => c.classList.remove("selected"));
-        // Add the 'selected' class to the clicked circle
-        circle.classList.add("selected");
-        selectedColor = parseInt(circle.textContent); // Set the selected color count
-    });
-});
+// Function to process the image using Vectorize API
+function processImage() {
+    let fileInput = document.getElementById('file-upload');
+    let image = fileInput.files[0];
 
-vectorizeButton.addEventListener("click", function() {
-    if (fileInput.files.length === 0) {
-        alert("Please upload an image first.");
+    if (!image) {
+        alert('Please upload an image first.');
         return;
     }
 
-    if (selectedColor === 0) {
-        alert("Please select the number of colors.");
+    if (selectedColorCount === 0) {
+        alert('Please select a color count.');
         return;
     }
 
-    const file = fileInput.files[0];
-    const formData = new FormData();
-    formData.append("image", file);
-    formData.append("processing.max_colors", selectedColor);
-    formData.append("output.file_format", "svg");
+    let formData = new FormData();
+    formData.append('image', image);
+    formData.append('processing.max_colors', selectedColorCount);
+    formData.append('output.file_format', 'svg');
 
-    fetch("https://api.vectorizer.ai/api/v1/vectorize", {
-        method: "POST",
+    fetch('https://api.vectorizer.ai/api/v1/vectorize', {
+        method: 'POST',
         headers: {
-            "Authorization": "Basic " + process.env.API_KEY
+            'Authorization': 'Basic ' + process.env.VECTORIZER_API_KEY
         },
         body: formData
     })
     .then(response => response.json())
     .then(data => {
         if (data && data.output && data.output.url) {
-            const svgElement = document.createElement("img");
-            svgElement.src = data.output.url;
-            imageSlider.appendChild(svgElement); // Display the "after" SVG image
+            // Display SVG beside the original image
+            let imageSlider = document.querySelector('.image-slider');
+            let svgElement = document.createElement('img');
+            svgElement.setAttribute('src', data.output.url);
+            imageSlider.appendChild(svgElement);
 
-            // Enable the download button
-            downloadButton.href = data.output.url;
-            downloadButton.style.display = "inline-block";
+            // Make SVG downloadable
+            let downloadBtn = document.getElementById('download-btn');
+            downloadBtn.href = data.output.url;
         } else {
-            alert("There was an error processing your image. Please try again.");
+            alert('Error vectorizing the image. Please try again.');
         }
     })
     .catch(error => {
-        console.error("Error:", error);
-        alert("An error occurred while vectorizing the image.");
+        alert('Error processing the image. Please try again.');
     });
-});
+}
+
+// Attach the processImage function to the Vectorize button
+document.getElementById('vectorize-btn').addEventListener('click', processImage);
