42e832ec8033da6c20c61ac514ef3a5f8f1c7b28
tyzima <ty@zimagear.com>
2023-09-04 13:16:45 -0400

    Updated vectorize-api.js for ES Module import

--- lambda/vectorize-api.js
+++ lambda/vectorize-api.js
@@ -1,44 +1,41 @@
 
-const { URLSearchParams } = require('url');
+const request = require('request');
 
-exports.handler = async (event, context) => {
+exports.handler = function(event, context, callback) {
     if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
+        callback(null, { statusCode: 405, body: 'Method Not Allowed' });
+        return;
     }
 
     const apiUrl = 'https://api.vectorizer.ai/api/v1/vectorize';
-    const apiHeaders = {
-        ...event.headers,
-        "Authorization": `Basic ${process.env.VECTORIZER_API_KEY}`
+
+    const formData = {
+        image: event.body // assuming the image is being sent as binary in the body
+        // TODO: Add more upload options here based on the API's parameters
     };
 
-    try {
-        const fetch = (await import('node-fetch')).default;
-
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            headers: apiHeaders,
-            body: event.body
-        });
-
-        // If response is not OK, return the response as text
-        if (!response.ok) {
-            return {
-                statusCode: response.status,
-                body: await response.text()
-            };
+    request.post({
+        url: apiUrl,
+        formData: formData,
+        auth: {user: process.env.VECTORIZER_API_KEY, pass: ''}, // Assuming API key is user
+        followAllRedirects: true,
+        encoding: null
+    }, function(error, response, body) {
+        if (error) {
+            callback(null, {
+                statusCode: 500,
+                body: `Error vectorizing the image: ${error.message}`
+            });
+        } else if (!response || response.statusCode != 200) {
+            callback(null, {
+                statusCode: response && response.statusCode,
+                body: body.toString('utf8')
+            });
+        } else {
+            callback(null, {
+                statusCode: 200,
+                body: body.toString('utf8') // Assuming the response is SVG or textual data
+            });
         }
-
-        // Return the JSON response
-        const data = await response.json();
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: `Error vectorizing the image: ${error.message}`
-        };
-    }
+    });
 };
